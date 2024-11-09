08bb56db84ddb997a7ff289525c5d5cc7310b742
udhayakumar <Udhayakumar>
2024-11-09 08:53:12 +0530

    specbeee

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const speakersSlider = document.getElementById("speakersSlider");
+
+    const openModal = (modalId) => {
+        const modal = document.getElementById(`${modalId}-modal`);
+        modal.classList.add("active");
+        speakersSlider.classList.add("shifted"); // Shift slider down
+    };
+
+    const closeModal = (modalId) => {
+        const modal = document.getElementById(`${modalId}-modal`);
+        modal.classList.remove("active");
+        speakersSlider.classList.remove("shifted"); // Reset slider position
+    };
+
+    window.openModal = openModal;
+    window.closeModal = closeModal;
+});
